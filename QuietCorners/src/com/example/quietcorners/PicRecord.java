6af3096187ab132d0ac5f2f1291ef7ccfd181947
sbewerse <sbewerse@mail.usf.edu>
2013-10-20 21:07:39 -0400

    Light Reading and PicRecord added.  Not pretty design for light reading.

---
+++ QuietCorners/src/com/example/quietcorners/PicRecord.java
@@ -0,0 +1,12 @@
+package com.example.quietcorners;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PicRecord extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picrecord);
+    }
+}
