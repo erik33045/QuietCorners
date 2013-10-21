f59b772441f87296824476aa5dd50f1bb13c4045
sbewerse <sbewerse@mail.usf.edu>
2013-10-20 20:59:27 -0400

    Light Reading and PicRecord added.  Not pretty design for light reading.

--- QuietCorners/src/com/example/quietcorners/LumenRecord.java
+++ QuietCorners/src/com/example/quietcorners/LumenRecord.java
@@ -1,12 +1,63 @@
 package com.example.quietcorners;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LumenRecord extends Activity {
+    TextView textMax, textReading;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lumenrecord);
+        textMax = (TextView)findViewById(R.id.max);
+        textReading = (TextView)findViewById(R.id.reading);
+
+        SensorManager sensorManager
+                = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        Sensor lightSensor
+                = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        if (lightSensor == null){
+            Toast.makeText(LumenRecord.this,
+                    "No Light Sensor! quit-",
+                    Toast.LENGTH_LONG).show();
+        }else{
+            float max =  lightSensor.getMaximumRange();
+            textMax.setText("   Max Reading: " + String.valueOf(max));
+
+            sensorManager.registerListener(lightSensorEventListener,
+                    lightSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+
+        }
     }
+
+    SensorEventListener lightSensorEventListener
+            = new SensorEventListener(){
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // TODO Auto-generated method stub
+            if(event.sensor.getType()==Sensor.TYPE_LIGHT){
+                float currentReading = event.values[0];
+                textReading.setText("   Current Reading: " + String.valueOf(currentReading));
+            }
+        }
+
+    };
 }
