e35dbe384f83fe32e8ca7d48ee778160b5af0ddc
sbewerse <sbewerse@mail.usf.edu>
2013-11-17 15:00:07 -0500

    Picture Debugging

--- QuietCorners/src/com/example/quietcorners/PicRecord.java
+++ QuietCorners/src/com/example/quietcorners/PicRecord.java
@@ -8,16 +8,19 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.widget.ImageView;
+import android.widget.Button;
+import android.view.View;
 
 import java.io.ByteArrayOutputStream;
 
 
 public class PicRecord extends Activity {
 
-    private ImageView cornerPicture;
-    public Bitmap image;
+    private ImageView image;
     public int picWidth;
     public int picHeight;
+    String encodedImageString;
+    Button addPictureButton;
 
 
     private void dispatchTakePictureIntent(int actionCode) {
@@ -25,16 +28,13 @@ public class PicRecord extends Activity {
         startActivityForResult(takePictureIntent, actionCode);
     }
 
-    private void handleSmallCameraPhoto(Intent intent) {
-        Bundle extras = intent.getExtras();
-        image = (Bitmap) extras.get("data");
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picrecord);
         dispatchTakePictureIntent(1);
+        addPictureButtonEvent();
+
     }
 
     @Override
@@ -47,11 +47,11 @@ public class PicRecord extends Activity {
             Bitmap bmp = (Bitmap) data.getExtras().get("data");
 
             image.setImageBitmap(bmp);
-            btnadd.requestFocus();
+            addPictureButton.requestFocus();
 
-            ByteArrayOutputStream outputstream = new ByteArrayOutputStream();
-            bmp.compress(Bitmap.CompressFormat.JPEG, 100, outputstream);
-            byte[] outputChunk = outputstream.toByteArray();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] outputChunk = baos.toByteArray();
             encodedImageString = Base64.encodeToString(outputChunk, Base64.DEFAULT);
 
             byte[] bytarray = Base64.decode(encodedImageString, Base64.DEFAULT);
@@ -62,25 +62,27 @@ public class PicRecord extends Activity {
 
     }
 
-    /*
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Bundle extras = data.getExtras();
-        mImageBitmap = (Bitmap) extras.get("data");
-        picWidth = mImageBitmap.getWidth();
-        picHeight = mImageBitmap.getHeight();
-        cornerPicture.setImageBitmap(mImageBitmap);
-        MediaStore.Images.Media.insertImage(getContentResolver(), mImageBitmap, "cornerImage", picDescription);
-    }*/
 
 
-    /*Button.OnClickListener mTakePicSOnClickListener =
-            new Button.OnClickListener() {
+    private void addPictureButtonEvent(){
+            Button button = (Button) findViewById(R.id.addPictureButton);
+            button.setOnClickListener(new View.OnClickListener()){
                @Override
-                public void onClick(View v) {
-                   dispatchTakePictureIntent(1);
+                       public void onClick(View view){
+                            Intent in = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            in.putExtra("crop", "true");
+                            in.putExtra("outputX", 100);
+                            in.putExtra("outputY", 100);
+                            in.putExtra("scale", true);
+                            in.putExtra("return-data", true);
+
+                            startActivityForResult(in, 1);
+                        }
+
                 }
-            };*/
+            };
 
 }
 
