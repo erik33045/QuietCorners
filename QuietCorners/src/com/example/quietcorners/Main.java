4a145d9418d9b38af80fc61205447a72a719f8af
Mike <mikrusfarr@gmail.com>
2013-11-18 10:48:10 -0500

    Interface for record and main updated.

--- QuietCorners/src/com/example/quietcorners/Main.java
+++ QuietCorners/src/com/example/quietcorners/Main.java
@@ -1,9 +1,7 @@
 package com.example.quietcorners;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
--- QuietCorners/src/com/example/quietcorners/WifiTest.java
+++ QuietCorners/src/com/example/quietcorners/WifiTest.java
@@ -6,11 +6,11 @@ import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.util.List;
 
 public class WifiTest extends Activity {
