b7aa9dc215019088f120a689928b0cba041fcb82
sbewerse <sbewerse@mail.usf.edu>
2013-11-17 21:39:28 -0500

    Confirmation Page and corner upload

--- QuietCorners/src/com/example/quietcorners/Confirm.java
+++ QuietCorners/src/com/example/quietcorners/Confirm.java
@@ -2,24 +2,56 @@ package com.example.quietcorners;
 
 import android.app.Activity;
 import android.content.*;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.graphics.Bitmap;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class Confirm extends Activity {
-    TextView lightReading;
     RatingBar overallRatingBar;
+    RatingBar quietRatingBar;
+    RatingBar lightRatingBar;
+    TextView hasOpenNetwork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
         GetCompleteButtonEvent();
-        overallRatingBar = (RatingBar)findViewById(R.id.overallRating);
+        Variables application = (Variables)getApplication();
+
+        //Sound Rating Bar
+        quietRatingBar = (RatingBar)findViewById(R.id.rtbQuietRating);
+        quietRatingBar.setRating(application.soundRating);
+        quietRatingBar.setFocusable(false);
+        quietRatingBar.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+        //Light Rating Bar
+        lightRatingBar = (RatingBar)findViewById(R.id.rtbLighRating);
+        lightRatingBar.setRating(application.lightRating);
+        lightRatingBar.setFocusable(false);
+        lightRatingBar.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+        //Over Rating Bar
+        overallRatingBar = (RatingBar)findViewById(R.id.rtbOverallRating);
         overallRatingBar.setStepSize(1);
+
+        //Wireless
+        hasOpenNetwork = (TextView)findViewById(R.id.txtHasOpenNetwork);
+        if(application.openNetwork) hasOpenNetwork.setText("YES");
+        else hasOpenNetwork.setText("NO");
     }
 
     @Override
