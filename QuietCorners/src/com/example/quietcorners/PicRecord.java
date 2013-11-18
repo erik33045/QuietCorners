6ffb5be675bbd219dbd10fafcbd74903fac2790e
Erik H <erik33045@gmail.com>
2013-11-18 01:59:56 -0500

    cgbssvda

--- QuietCorners/src/com/example/quietcorners/PicRecord.java
+++ QuietCorners/src/com/example/quietcorners/PicRecord.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
